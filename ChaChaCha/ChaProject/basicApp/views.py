ed8f634019844a874a15ec09bdcd19c38dc81bc0
he-reme <rlagpfla1017@naver.com>
2021-02-08 10:21:27 +0900

    후기게시판 업로드1

--- ChaChaCha/ChaProject/basicApp/views.py
+++ ChaChaCha/ChaProject/basicApp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+# 홈 화면
 def home(request):
     return render(request, 'home.html', None)
+
 
 def contact(request) :
     return render(request, 'contact.html', None)
